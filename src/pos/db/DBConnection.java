12a954628471f9c3922e15c165fd19e91655b985
mangana98 <manganacollins13983@gmail.com>
2026-05-08 18:05:10 +0300

    Modify DBConnection credentials and host variable
    Updated the database connection credentials to use an empty password and changed the host variable to a lowercase type.

--- src/pos/db/DBConnection.java
+++ src/pos/db/DBConnection.java
@@ -4,10 +4,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnection {
-
+    private static final string HOST = "STEPHEN-PC"
     private static final String URL = "jdbc:mysql://localhost:3306/pos_system";
     private static final String USER = "root";
-    private static final String PASS = "1234";
+    private static final String PASS = "";
 
     public static Connection getConnection() throws Exception {
         return DriverManager.getConnection(URL, USER, PASS);
